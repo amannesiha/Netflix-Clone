634b212c18d56d35c554f0fb28892777e09d3549
abukic3 <50766285+abukic3@users.noreply.github.com>
2024-08-19 20:47:18 -0700

    banner

--- netflix-clone/src/Utils/requests.js
+++ netflix-clone/src/Utils/requests.js
@@ -1,8 +1,9 @@
 // const API_KEY = "647ab1e82914cecf27dd747c4696378b";
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
   fetchTrending: `/trending/all/week?api_key=$(API_KEY)&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=$(API_KEY)&with_networks=123`,
+  // fetchNetflixOriginals: `/discover/tv?api_key=$(API_KEY)&with_networks=123`,
+  fetchNetflixOriginals: `/discover/tv?api_key=647ab1e82914cecf27dd747c4696378b&with_networks=123`,
   fetchTopRatedMovies: `/movie/top_rated?api_key=$(API_KEY)&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=$(API_KEY)&with_genres=28`,
   fetchComedyMovie: `/discover/movie?api_key=$(API_KEY)&with_genres=35`,
